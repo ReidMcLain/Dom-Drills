94a21286a772718e9e34c8ccd78dc02aa1d71dca
ReidMcLain <reidmm@gmail.com>
2022-02-10 10:52:54 -0600

    commit message

---
+++ app.js
@@ -0,0 +1,83 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const newDiv = document.createElement("div");
+    newDiv.className = "header-container"
+
+    const newH1 = document.createlement("h1");
+    newH1.className = "h1";
+    const h1TextNode = document.createTextNode("This is an h1!");
+
+    const newH2 = document.createlement("h2");
+    newH2.className = "h2";
+    const h2TextNode = document.createTextNode("This is an h2!");
+
+    const newH3 = document.createlement("h3");
+    newH2.className = "h3";
+    const h3TextNode = document.createTextNode("This is an h3!");
+
+    const newH4 = document.createlement("h4");
+    newH2.className = "h4";
+    const h4TextNode = document.createTextNode("This is an h4!");
+
+    const newH5 = document.createlement("h5");
+    newH2.className = "h5";
+    const h5TextNode = document.createTextNode("This is an h5!");
+
+    const newH6 = document.createlement("h6");
+    newH2.className = "h6";
+    const h6TextNode = document.createTextNode("This is an h6!");
+
+    newH1.appendChild(h1TextNode);
+    newH2.appendChild(h2TextNode);
+    newH3.appendChild(h3TextNode);
+    newH4.appendChild(h4TextNode);
+    newH5.appendChild(h5TextNode);
+    newH6.appendChild(h6TextNode);
+
+    newH1.addEventListener("click", setRandomColor);
+    newH2.addEventListener("click", setRandomColor);
+    newH3.addEventListener("click", setRandomColor);
+    newH4.addEventListener("click", setRandomColor);
+    newH5.addEventListener("click", setRandomColor);
+    newH6.addEventListener("click", setRandomColor);
+
+    newDiv.appendChild(newH1);
+    newDiv.appendChild(newH2);
+    newDiv.appendChild(newH3);
+    newDiv.appendChild(newH4);
+    newDiv.appendChild(newH5);
+    newDiv.appendChild(newH6);
+
+    document.body.appendChild(newDiv);
+
+    const colorArr = [
+        "black",
+        "pink",
+        "purple",
+        "gold",
+        "goldenrod",
+        "red",
+        "blue",
+        "yellow"
+    ];
+
+    const listItemButton = document.querySelector("list-button");
+    let listCount = 0;
+
+    listItemButton.addEventListener("click", function () {
+        const newLi = document.createlement("li");
+        listCount++
+        newLi.textContent = "This is the list item " + listCount;
+        newLi.addEventListener("click", setRandomColor);
+        newLi.addEventListener("dblclick", function () {
+            this.remove();
+        });
+
+        document.body.appendChild(newLi);
+    });
+
+    function setRandomColor(event) {
+        const randomNum = Math.floor(Math.random() * colorArr.length);
+        event.target.style.color = colorArr[randomNum];
+    }
+});
